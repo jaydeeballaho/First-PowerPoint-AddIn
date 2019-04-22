6017053291ff0565435c041f39716337b445be58
jaydeegeek <jaydie.ballaho@gmail.com>
2019-04-22 19:08:48 +0800

    updated displayed text in textbox.

--- FirstPowerPointAddIn/ThisAddIn.cs
+++ FirstPowerPointAddIn/ThisAddIn.cs
@@ -25,7 +25,7 @@ namespace FirstPowerPointAddIn
         {
             PowerPoint.Shape textBox = Sld.Shapes.AddTextbox(
                Office.MsoTextOrientation.msoTextOrientationHorizontal, 0, 0, 500, 50);
-            textBox.TextFrame.TextRange.InsertAfter("This text was added by using code.");
+            textBox.TextFrame.TextRange.InsertAfter("This text was added by using code initially.");
         }
 
         #region VSTO generated code
